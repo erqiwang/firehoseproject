cc15fa4c755bebbba1db1271cfb29db3795dde8d
erqiwang <vagrant@precise32.(none)>
2014-05-11 23:10:38 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Erqi::Application.config.secret_key_base = '695bc92edda0930492a25b1164f7943f3edeb0401b83aa1ab82104d094a9a1eccac43dfb540fbc6546236ca29b81ecf7035fae1463fd4dd7b17a7837ef2f4a08'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Erqi::Application.config.session_store :cookie_store, key: '_erqi_session'
